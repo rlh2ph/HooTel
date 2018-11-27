b2327c9bd59420421895132d9afd7ca74c5d0392
amb6sg <amb6sg@virginia.edu>
2018-11-26 22:38:13 -0500

    added red id var to person res search

--- reservationPerPerson.php
+++ reservationPerPerson.php
@@ -111,6 +111,7 @@ function submit($lastName,$mysqli){
         $room_num = $new_row['room_num'];
         $check_in = $new_row['check_in'];
         $check_out = $new_row['check_out'];
+        $red_id = $new_row['res_id'];
         echo $guest_first_name . " " . $guest_last_name;
         echo "<br>";
         echo date("m/d/Y", strtotime($check_in)) . " - " . date("m/d/Y", strtotime($check_out));

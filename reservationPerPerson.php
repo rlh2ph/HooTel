5312acc42b68d3cfc2ec650f542fcf19b5000f01
amb6sg <amb6sg@virginia.edu>
2018-11-26 23:24:09 -0500

    tiny change

--- reservationPerPerson.php
+++ reservationPerPerson.php
@@ -34,6 +34,7 @@ $mysqli = new mysqli("mysql.cs.virginia.edu", "am7eu", "u9KzwMUi", "am7eu_dbproj
   <body>
 
 <?php
+$Err = "";
 $lastName = "";
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
   if (empty($_POST["last_name"])) {
@@ -53,9 +54,10 @@ function test_input($data) {
   $data = htmlspecialchars($data);
   return $data;
 }
+
 if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['submit']))
 {
-    $guest_result = submit($lastName,$mysqli);
+    $guest_result = submit($_POST['last_name'],$mysqli);
 }
 
 ?>

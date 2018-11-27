e6317f1005ec25fb9a1b3c783b47c7c57d94aff0
magghunt <mmh4sb@virginia.edu>
2018-11-26 23:58:57 -0500

    beautiful css

--- cancelReservation.php
+++ cancelReservation.php
@@ -7,6 +7,35 @@
 </head>
 <body>
 
+  <head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <title>The HooTel</title>
+
+<link rel="shortcut icon" href="img/favicon.png" type="image/x-icon"/>
+
+  <!-- Bootstrap core CSS -->
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+  <!-- Plugin CSS -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/creative.min.css" rel="stylesheet">
+
+<!--Our own css -->
+<link href="reservation.css" rel="stylesheet">
+
+</head>
+  <body>
+
 <?php
 $conn = new mysqli("mysql.cs.virginia.edu", "am7eu", "u9KzwMUi", "am7eu_dbproject");
 ?>
@@ -41,6 +70,8 @@ function test_input($data) {
 </form>
 
 <?php
+
+include(dirname(__FILE__).'/components/nav.php');
 
 if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['submit']))
 {
